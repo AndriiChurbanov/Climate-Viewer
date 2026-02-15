548e4ca7b532dae548831c7d2cfa2a187b1716a8
AndriiChurbanov <117305969+AndriiChurbanov@users.noreply.github.com>
2026-02-16 00:50:05 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,5 +13,6 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+    expect(find.byType(MyApp), findsOneWidget);
   });
 }
